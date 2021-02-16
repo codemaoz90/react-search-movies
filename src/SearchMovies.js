4154a8487c3f1c69a71a312721250e57aa78a401
codemaoz90 <codemaoz@gmail.com>
2021-02-16 01:55:26 +0100

    Functionality done, filter and map movies

--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 export default function SearchMovies() {
 	const [query, setQuery] = useState("");
+	const [movies, setMovies] = useState([]);
+
 	const searchMovies = async (e) => {
 		e.preventDefault();
 		// console.log("Submitted");
@@ -8,28 +10,57 @@ export default function SearchMovies() {
 		try {
 			const res = await fetch(url);
 			const data = await res.json();
-			console.log(data);
+			setMovies(data.results);
 		} catch (error) {
 			console.log(error);
 		}
 	};
 
 	return (
-		<form className="form">
-			<label className="label" htmlFor="query">
-				Movie Name:
-			</label>
-			<input
-				type="text"
-				name="query"
-				placeholder="i.e. Jurassic Park"
-				className="input"
-				value={query}
-				onChange={(e) => setQuery(e.target.value)}
-			/>
-			<button className="button" type="submit" onClick={(e) => searchMovies(e)}>
-				SUBMIT
-			</button>
-		</form>
+		<>
+			<form className="form">
+				<label className="label" htmlFor="query">
+					Movie Name:
+				</label>
+				<input
+					type="text"
+					name="query"
+					placeholder="i.e. Jurassic Park"
+					className="input"
+					value={query}
+					onChange={(e) => setQuery(e.target.value)}
+				/>
+				<button
+					className="button"
+					type="submit"
+					onClick={(e) => searchMovies(e)}
+				>
+					SUBMIT
+				</button>
+			</form>
+			<div className="card-list">
+				{movies
+					.filter((movie) => movie.poster_path)
+					.map((movie) => (
+						<div className="card" key={movie.id}>
+							<img
+								className="card--image"
+								src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+								alt={movie.title + " poster"}
+							/>
+							<div className="card--content">
+								<h3 className="card--title">{movie.title}</h3>
+								<p>
+									<small>RELEASE DATE: {movie.release_date}</small>
+								</p>
+								<p>
+									<small>RATING: {movie.vote_average}</small>
+								</p>
+								<p className="card--desc">{movie.overview}</p>
+							</div>
+						</div>
+					))}
+			</div>
+		</>
 	);
 }
